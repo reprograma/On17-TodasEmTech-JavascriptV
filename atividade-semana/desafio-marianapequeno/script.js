a6a7f69f2aa918b475fd03d6816ad39217cfbe74
Mariana Pequeno <pequeno.mariana@gmail.com>
2022-06-16 11:58:44 -0300

    alterações

--- atividade-semana/desafio-marianapequeno/script.js
+++ atividade-semana/desafio-marianapequeno/script.js
@@ -3,20 +3,20 @@ const form = document.querySelector("form");
 const input = document.querySelector("#name");
 const btn = document.querySelector("button");
 
-btn.addEventListener("click", handleClick);
+// btn.addEventListener("click", handleClick);
 
-function handleClick(e){
-  e.preventDefault()
-  searchDigimon(input.value); //Koromon
-  // form.reset();
-  console.log(input.value);
-}
+// function handleClick(e){
+//   e.preventDefault()
+//   searchDigimon(input.value); //Koromon
+//   // form.reset();
+//   console.log(input.value);
+// }
 
-async function searchDigimon(name){
+async function searchDigimon(){
   try{
-    const response = await fetch(`https://digimon-api.vercel.app/api/digimon/name/${name}`);
+    const response = await fetch(`https://digimon-api.vercel.app/api/digimon`);
     const dataDigimon = await response.json();
-
+    
     const container = document.createElement("div");
     mainContent.appendChild(container);
     container.classList.add("container");
@@ -30,11 +30,18 @@ async function searchDigimon(name){
     container.appendChild(image);
     image.classList.add("image");
 
-    titleName.innerText = `Nome: ${dataDigimon.name}`;
-    level.innerHTML = `<strong>Level</strong>: ${dataDigimon.level}`;
-    image.src = dataDigimon.img;   
+    dataDigimon.forEach(element => {
+      console.log(element.name)
+      titleName.innerText = `Nome: ${element.name}`;
+      level.innerHTML = `<strong>Level</strong>: ${element.level}`;
+      image.src = element.img;   
+    });
+
+    
   }
   catch(e){
     console.error(e);
   }
 }
+
+searchDigimon()
