a2506ca40deb43a632a3d1184bfe08adfab26e1d
Mariana Pequeno <pequeno.mariana@gmail.com>
2022-06-19 12:21:16 -0300

    Finalização da atividade

--- atividade-semana/desafio-marianapequeno/script.js
+++ atividade-semana/desafio-marianapequeno/script.js
@@ -3,45 +3,50 @@ const form = document.querySelector("form");
 const input = document.querySelector("#name");
 const btn = document.querySelector("button");
 
-// btn.addEventListener("click", handleClick);
+btn.addEventListener("click", handleClick);
 
-// function handleClick(e){
-//   e.preventDefault()
-//   searchDigimon(input.value); //Koromon
-//   // form.reset();
-//   console.log(input.value);
-// }
+function handleClick(e){
+  e.preventDefault()
+  // console.log(input.value);
+  const nameDigimon = input.value; //Koromon
+  searchDigimon(`https://digimon-api.vercel.app/api/digimon/name/${nameDigimon}`); 
 
-async function searchDigimon(){
-  try{
-    const response = await fetch(`https://digimon-api.vercel.app/api/digimon`);
-    const dataDigimon = await response.json();
-    
-    const container = document.createElement("div");
-    mainContent.appendChild(container);
-    container.classList.add("container");
-    const titleName = document.createElement("h2");
-    container.appendChild(titleName);
-    titleName.classList.add("title-nome");
-    const level = document.createElement("span");
-    container.appendChild(level);
-    level.classList.add("level");
-    const image = document.createElement("img");
-    container.appendChild(image);
-    image.classList.add("image");
+  const container = document.createElement("div");
+  mainContent.appendChild(container);
+  container.classList.add("container");
+  const titleName = document.createElement("h2");
+  container.appendChild(titleName);
+  titleName.classList.add("title-nome");
+  const level = document.createElement("span");
+  container.appendChild(level);
+  level.classList.add("level");
+  const image = document.createElement("img");
+  container.appendChild(image);
+  image.classList.add("image");
 
-    dataDigimon.forEach(element => {
-      console.log(element.name)
-      titleName.innerText = `Nome: ${element.name}`;
-      level.innerHTML = `<strong>Level</strong>: ${element.level}`;
-      image.src = element.img;   
-    });
-
-    
-  }
-  catch(e){
-    console.error(e);
+  async function searchDigimon(url){
+    try{
+      const response = await fetch(url, 
+        // {
+      // headers: {
+      //   'Content-Type': 'application/json'
+      //   }
+      // }
+      )
+      const dataDigimon = await response.json();
+      console.log(dataDigimon)
+     
+      dataDigimon.forEach(element => {
+        // console.log(element)
+        titleName.innerText = `Nome: ${element.name}`;
+        level.innerHTML = `<strong>Level</strong>: ${element.level}`;
+        image.src = element.img;   
+      });
+    }
+    catch(e){
+      console.error(e)
+    }
   }
 }
 
-searchDigimon()
+
