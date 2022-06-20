007ce06e5eecb59a522f3fef65d07fea46c07645
Letícia Coelho <coelho.letticia@gmail.com>
2022-06-20 15:18:09 -0300

    desafio digimon

--- hackearAPI/Leticia-Coelho/main.js
+++ hackearAPI/Leticia-Coelho/main.js
@@ -1,55 +1,55 @@
-
-const url = "https://digimon-api.vercel.app/api/digimon";
-const api = axios.create({ baseURL: url });
-
-const searchName = document.getElementById("txtBusca");
-const btnBusca = document.getElementById("btnBusca");
-const digimonImageEl = document.getElementById("digimonImage");
-const digimonNameEl = document.getElementById("digimonName");
+const input = document.getElementById('input');
+const botao = document.getElementById('button');
 
 
+botao.addEventListener('click', (e) => {
+  e.preventDefault();
+  getDigimons(input.value);
+  input.value = '';
+});
 
-async function fetchDigimonByName(name) {
-    try {
-      const response = await api.get(`/name/${name}`);
-      return response.data;
-    } catch (err) {
-      alert("Ops! Não encontramos esse digimon!");
-      return [];
-    }
+const getDigimons = async (name) => {
+const url = `https://digimon-api.vercel.app/api/digimon/name/${name}`;
+  try {
+    const response = await fetch(url);
+      if (response.status == '200') {
+        const data = await response.json();
+        const titulo = document.querySelector('h2');
+        titulo.innerText = data[0].name;
+        const cards = document.querySelector('.cards');
+        cards.appendChild(titulo);
+        const nivel = document.querySelector('p');
+        nivel.innerText = data[0].level;
+        cards.appendChild(nivel);
+        const imagem = document.querySelector('img');
+        imagem.src = data[0].img;
+        cards.appendChild(imagem);
+
+  } else {
+    alert ("Este Digimon não existe!");
+    throw new Error();
   }
+}
+  catch (err) {
+      console.error("Não foi possível prosseguir com a requisição", err);
+  }
+};
 
-  async function fetchSearchedDigimon(searchType) {
-    const digimonName = searchName.value.trim();
-  
-    let digimons = [];
-  
-    if (searchType === "name") {
-      digimons = await fetchDigimonByName(digimonName);
-    } else {
-        return [];
-    }
+const listaDigimons = async () => {
+  try {
+    const resposta = await fetch(`https://digimon-api.vercel.app/api/digimon/`
+);
+const data = await resposta.json()
+const listagem = document.querySelector('.lista');
+    data.map((digimon)=> {
+      const name = document.createElement('p');
+      name.innerText = digimon.name;
+      listagem.appendChild(name);
+    })
+  }
+  catch (err) {
+    console.error(err);
+  }
+};
 
-    if (digimons.length > 0) {
-        const randomIndex = Math.floor(Math.random() * digimons.length);
-        const { name, img,} = digimons[randomIndex];
-    
-        digimonImageEl.setAttribute("src", img);
-        digimonImageEl.setAttribute("alt", `Imagem do digimon ${name}`);
-        digimonImageEl.setAttribute("title", `Imagem do digimon ${name}`);
-        digimonImageEl.style.visibility = "visible";
-    
-        digimonNameEl.innerText = name;
-        digimonNameEl.style.textAlign = "center";
-        digimonNameEl.style.color = "white";
-        digimonLevelEl.style.textAlign = "center";
-      }
-    }
-
-    btnBusca.addEventListener("click", () => fetchSearchedDigimon("name"));
-
-    searchName.addEventListener("keypress", function (event) {
-        if (event.key === "Enter") {
-          fetchSearchedDigimon("name");
-        }
-      });
+listaDigimons() 
