1f5d443515505acd516683e176edb9a979627549
Catharine Augusto <94121842+yeahcath@users.noreply.github.com>
2022-07-11 22:08:59 -0300

    uhullll

---
+++ revisao/script.js
@@ -0,0 +1,9 @@
+console.log("Olá Mundo")
+
+function chamarPessoa() {
+
+    setTimeout(() => { "Ellie"} 2000)
+} 
+
+// then espera o status da promise e captura o argumento da callback que é o response
+
